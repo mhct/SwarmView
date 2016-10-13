4b3fe7e84351ce7c84fe3b1be91a0c9ec26a1335
mariohct <mario.hct@gmail.com>
2016-10-13 21:29:52 +0200

    Fixed the zoom and rotation of scene

--- src/main/RatsView.java
+++ src/main/RatsView.java
@@ -14,7 +14,9 @@ public class RatsView extends PApplet {
 	Drone[] drones = new Drone[NUMBER_DRONES];
 	private float timeStep;
 	int rotzfactor = 0;
-	float zoom = 0;
+	float zoom = 1.0f;
+	final int displayDimensionX = 1024;
+	final int displayDimensionY = 800;
 	
 	public static void main(String[] args) {
 		PApplet.main(RatsView.class);
@@ -23,7 +25,7 @@ public class RatsView extends PApplet {
 	@Override
 	public void settings() {
 //		fullScreen();
-		size(600, 600, P3D);
+		size(displayDimensionX, displayDimensionY, P3D);
 	}
 	
 	@Override
@@ -47,34 +49,26 @@ public class RatsView extends PApplet {
 		background(0);
 		
 		Point mouse = MouseInfo.getPointerInfo().getLocation();
-		zoom = constrain(zoom, 1.0f, 2.0f);
-
+		zoom = constrain(zoom, 0.3f, 4.0f);
 		scale(zoom);
-		/**
-		 * Rotation on the screen
-		 */
-//		float rotx = (mouseY/600.0f)*-2*PI+PI;
-//		float roty = (mouseX/600.0f)*2*PI-PI;
-//		float rotz = rotzfactor*PI/36;
-		float rotx = (2*mouse.x/600.0f)*-2*PI+PI;
-		float roty = (2*mouse.y/600.0f)*-2*PI-PI;
-		float rotz = rotzfactor*PI/36;
 		
-//		translate(0, 0, rotz);
+		/**
+		 * Rotation of the screen
+		 */
+		float rotx = (2*mouse.x/(float) displayDimensionX)*-2*PI+PI;
+		float roty = (2*mouse.y/(float) displayDimensionY)*-2*PI-PI;
+		float rotz = rotzfactor*PI/36;
 		
 		pushMatrix();
 		
-		//TODO fiz dimensions to dimensions of the room. Check coordinates from trajectory, and units
-//		camera(mouseX, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
-//		scale(zoom, zoom, zoom not working
-		translate(width/2, height/2);
+		translate(width/(2*zoom), height/(2*zoom), rotz/zoom);
 		rotateX(roty);  
 		rotateZ(rotx);  
 		
 		pushMatrix();
 		strokeWeight(2.0f);
 		translate(0, 0, 100);
-		box(width/2, width/2, 200);
+		box(width/1.2f, width/1.2f, 200);
 		popMatrix();
 		
 		pushMatrix();
@@ -84,32 +78,17 @@ public class RatsView extends PApplet {
 		for (int i=0; i<NUMBER_DRONES; i++) {
 			drones[i].displayNext(timeStep);
 		}
-		
 		popMatrix();
-		
-		
+
 		timeStep = timeStep + 1.0f;
-	}
-	
-	@Override
-	public void keyPressed() {
-		if (key == CODED) {
-			if (keyCode == UP) {
-				rotzfactor++;
-				
-		    }
-			else if (keyCode == DOWN) {
-				rotzfactor--;
-		    	}
-		}
 	}
 	
 	public void mouseWheel(MouseEvent event) {
 		if(event.getCount() >= 0) { 
-		    zoom += 0.005; 
+		    zoom += 0.05; 
 		  } 
 		  else {
-		    zoom -= 0.01; 
+		    zoom -= 0.05; 
 		  }
 	}
 	
@@ -132,48 +111,6 @@ public class RatsView extends PApplet {
 		vertex( 0, -1,  1);
 		vertex( 1,  -1,  0);
 		endShape();
-				
-		
-		  // Back
-//		  beginShape(QUADS);
-//		  fill(255,255,0);
-//		  vertex( 1, -1, -1);
-//		  vertex(-1, -1, -1);
-//		  vertex(-1,  1, -1);
-//		  vertex( 1,  1, -1);
-//		  endShape();
-//		  // Bottom
-//		  beginShape(QUADS);
-//		  fill( 255,0,255);
-//		  vertex(-1,  1,  1);
-//		  vertex( 1,  1,  1);
-//		  vertex( 1,  1, -1);
-//		  vertex(-1,  1, -1);
-//		  endShape();
-//		  // Top
-//		  beginShape(QUADS);
-//		  fill(0,255,0);
-//		  vertex(-1, -1, -1);
-//		  vertex( 1, -1, -1);
-//		  vertex( 1, -1,  1);
-//		  vertex(-1, -1,  1);
-//		  endShape();
-//		  // Right
-//		  beginShape(QUADS);
-//		  fill(0,0,255);
-//		  vertex( 1, -1,  1);
-//		  vertex( 1, -1, -1);
-//		  vertex( 1,  1, -1);
-//		  vertex( 1,  1,  1);
-//		  endShape();
-//		  // Left
-//		  beginShape(QUADS);
-//		  fill(0,255,255);
-//		  vertex(-1, -1, -1);
-//		  vertex(-1, -1,  1);
-//		  vertex(-1,  1,  1);
-//		  vertex(-1,  1, -1);
-//		  endShape();
 	}
 
 }
