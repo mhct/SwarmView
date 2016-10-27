b13f2f61c8740b103f69440bfa33919fa252c688
Tom Holvoet <Tom.Holvoet@cs.kuleuven.be>
2016-10-27 10:26:00 +0200

    refactored NerveIntro

--- src/main/java/rats/acts/introduction/IntroductionAct.java
+++ src/main/java/rats/acts/introduction/IntroductionAct.java
@@ -35,7 +35,7 @@ public class IntroductionAct extends Act {
 		
 		try {
 			//TODO Parsing the trajectories configuration will be added here
-			act.addTrajectory(Nerve, new NerveTrajectoryIntroduction(act.initialPosition(Nerve), act.finalPosition(Nerve)));
+			act.addTrajectory(Nerve, new NerveTrajectoryIntroduction2(act.initialPosition(Nerve), act.finalPosition(Nerve)));
 			act.addTrajectory(Romeo, TwinDrones.createRomeoTrajectory());
 			act.addTrajectory(Juliet, TwinDrones.createJulietTrajectory());
 			act.addTrajectory(Fievel, IntroductionAct.exampleLineTrajectory(act.initialPosition(Fievel), act.finalPosition(Fievel), act.getDuration()));
