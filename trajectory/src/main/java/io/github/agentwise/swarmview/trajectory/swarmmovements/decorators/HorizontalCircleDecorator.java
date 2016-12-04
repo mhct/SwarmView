f64449a5570ce3b07552e4000bf8b76f78c4b7d7
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-12-04 22:40:48 +0100

    Update HorizontalCircleDecorator

--- trajectory/src/main/java/io/github/agentwise/swarmview/trajectory/swarmmovements/decorators/HorizontalCircleDecorator.java
+++ trajectory/src/main/java/io/github/agentwise/swarmview/trajectory/swarmmovements/decorators/HorizontalCircleDecorator.java
@@ -9,12 +9,17 @@ public class HorizontalCircleDecorator implements FiniteTrajectory4d {
   private final FiniteTrajectory4d trajectory;
   private final Point3D center;
   private final double frequency;
+  private final double phase;
 
   private HorizontalCircleDecorator(
       FiniteTrajectory4d trajectory, Point3D center, double frequency) {
     this.trajectory = trajectory;
     this.center = Point3D.create(center.getX(), center.getY(), 0);
     this.frequency = frequency;
+    final Pose firstPose = trajectory.getDesiredPosition(0);
+    final double dx = firstPose.x() - center.getX();
+    final double dy = firstPose.y() - center.getY();
+    phase = StrictMath.atan2(dy, dx);
   }
 
   public static HorizontalCircleDecorator create(
@@ -32,8 +37,10 @@ public class HorizontalCircleDecorator implements FiniteTrajectory4d {
     final Pose initialPose = trajectory.getDesiredPosition(timeInSeconds);
     final double radius =
         Point3D.distance(Point3D.create(initialPose.x(), initialPose.y(), 0), center);
-    final double shiftX = radius * StrictMath.sin(2 * StrictMath.PI * frequency * timeInSeconds);
-    final double shiftY = radius * StrictMath.cos(2 * StrictMath.PI * frequency * timeInSeconds);
+    final double shiftX =
+        radius * StrictMath.sin(2 * StrictMath.PI * frequency * timeInSeconds + phase);
+    final double shiftY =
+        radius * StrictMath.cos(2 * StrictMath.PI * frequency * timeInSeconds + phase);
     final Pose decoratedPose =
         Pose.create(
             initialPose.x() + shiftX, initialPose.y() + shiftY, initialPose.z(), initialPose.yaw());
