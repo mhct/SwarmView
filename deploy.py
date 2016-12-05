ec4f2ffc7715a51a079af1500e6bc1d040cda55c
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-12-05 16:55:33 +0100

    Add deploy script

---
+++ deploy.py
@@ -0,0 +1,12 @@
+import subprocess
+import shlex
+
+
+def execute_cmd(cmd):
+    subprocess.call(shlex.split(cmd))
+
+
+if __name__ == '__main__':
+    execute_cmd('gradle clean')
+    execute_cmd('gradle build -x test')
+    execute_cmd('scp -oProxyCommand="ssh -W %h:%p charmer" trajectory/build/libs/swarmview-trajectory-1.1.45.jar virt-controller@virtual-charmer1:/home/virt-controller')
