4665b9fe07ad8e619bde82d0613198b934861a2c
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-12-29 21:03:01 +0100

    Update Fievel intro act

--- trajectory/src/main/java/io/github/agentwise/swarmview/trajectory/rats/acts/introduction/FievelIntroduction.java
+++ trajectory/src/main/java/io/github/agentwise/swarmview/trajectory/rats/acts/introduction/FievelIntroduction.java
@@ -18,10 +18,10 @@ public class FievelIntroduction {
     if (start > 0) {
       fievel.hover(start);
     }
-    fievel.moveTriangleToPoint(Point4D.create(6, 3, 3, YAW), 1, 1);
-    fievel.moveTriangleToPoint(Point4D.create(0, 3.5, 3, YAW), 1, 1);
-    fievel.moveTriangleToPoint(Point4D.create(6, 4, 3, YAW), 1, 1);
-    fievel.moveTriangleToPoint(Point4D.create(0, 4, 3, YAW), 1, 1);
+    fievel.moveTriangleToPoint(Point4D.create(5.5, 3, 3, YAW), 1, 1);
+    fievel.moveTriangleToPoint(Point4D.create(0.5, 3.5, 3, YAW), 1, 1);
+    fievel.moveTriangleToPoint(Point4D.create(5.5, 4, 3, YAW), 1, 1);
+    fievel.moveTriangleToPoint(Point4D.create(0.5, 4, 3, YAW), 1, 1);
     fievel.moveTriangleToPoint(Point4D.create(5.5, 3.8, 3, YAW), 1, 1);
     fievel.moveTriangleToPoint(Point4D.create(1.0, 3.8, 3, YAW), 1, 1);
     fievel.moveTriangleToPoint(Point4D.from(finalPose), 1, 1);
