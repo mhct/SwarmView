3d8eb265296deebd8959534125d4a6977562c98f
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-12-29 20:58:09 +0100

    Update initial pos and chaos

--- trajectory/src/main/java/io/github/agentwise/swarmview/trajectory/rats/RatsShow.java
+++ trajectory/src/main/java/io/github/agentwise/swarmview/trajectory/rats/RatsShow.java
@@ -38,13 +38,13 @@ public class RatsShow {
             DroneName.Nerve, Pose.create(6, 4, 1.0, YAW), Pose.create(2.0, 1.0, 3.0, YAW)));
     introPositions.add(
         DronePositionConfiguration.create(
-            DroneName.Romeo, Pose.create(0.0, 2.0, 1.0, YAW), Pose.create(5.0, 5.0, 1.0, YAW)));
+            DroneName.Romeo, Pose.create(0.5, 2.0, 1.0, YAW), Pose.create(5.0, 5.0, 1.0, YAW)));
     introPositions.add(
         DronePositionConfiguration.create(
             DroneName.Juliet, Pose.create(1.0, 1.0, 1.0, YAW), Pose.create(5.7, 5.0, 1.0, YAW)));
     introPositions.add(
         DronePositionConfiguration.create(
-            DroneName.Fievel, Pose.create(0.0, 5.0, 1.0, YAW), Pose.create(3.5, 1.0, 1.5, YAW)));
+            DroneName.Fievel, Pose.create(0.5, 5.0, 1.0, YAW), Pose.create(3.5, 1.0, 1.5, YAW)));
     introPositions.add(
         DronePositionConfiguration.create(
             DroneName.Dumbo, Pose.create(5., 1.0, 1.0, YAW), Pose.create(4.0, 3.5, 2.5, YAW)));
