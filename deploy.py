996c368e3e5addbb28de8237ae9df8720f6706d3
Hoang Tung Dinh <tunghdinh@gmail.com>
2017-01-07 19:06:43 +0100

    Update deploy script

--- deploy.py
+++ deploy.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     execute_cmd('gradle clean')
     execute_cmd('gradle build -x test --offline')
     execute_cmd('scp trajectory/build/libs/swarmview-trajectory-1.1.45.jar controller@charmer.local:/home/controller/rats/rats_ws/src/rats/BeSwarm/libs')
-    execute_cmd('scp trajectory/build/libs/swarmview-trajectory-1.1.45.jar tung@vaio.local:/home/tung/rats/rats_ws/src/rats/BeSwarm/libs')
+    execute_cmd('scp trajectory/build/libs/swarmview-trajectory-1.1.45.jar controller@supercharmer.local:/home/controller/rats/rats_ws/src/rats/BeSwarm/libs')
